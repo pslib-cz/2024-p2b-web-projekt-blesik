0f6b9a79408e1ee8ad15336127839fa5fb411069
blesik <jan.blesa.023@pslib.cz>
2025-05-13 15:57:20 +0200

    1

--- src/scripts/hamburger.js
+++ src/scripts/hamburger.js
@@ -1,11 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let hamelmnts = document.querySelectorAll(".hamburger-zone");
-    for (const btn of document.querySelectorAll(".hamburger-btn")) {
-        btn.addEventListener("click", (e) => {
-            e.preventDefault();
-            for (const element of hamelmnts) {
-                element.classList.toggle("active");
-            }  
-        });
-    }
-});
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const navMenu = document.getElementById('nav-menu');
+  
+    hamburger.addEventListener('click', () => {
+      navMenu.classList.toggle('active');
+    });
+  });
